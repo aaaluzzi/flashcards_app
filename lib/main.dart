40e030788279a6f6b79f4f5b8418708543937d3c
Maestroarm <artakvanyan@gmail.com>
2023-08-06 15:49:17 -0700

    Revert "Added delete flashcard row"
    This reverts commit 6e06961b4a23aebb4234f0d6191286a124a7910a.

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,6 @@ class FlashcardsState extends ChangeNotifier {
     flashcards.add(flashcard);
     notifyListeners();
   }
-  void removeFlashcard(String term) {
-    flashcards.removeWhere((flashcard) => flashcard.term == term);
-    notifyListeners();
-  }
 }
 
 void main() {
@@ -36,13 +32,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flashcards',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flashcards'),
+      home: const MyHomePage(title: 'Flashcards Home Page'),
     );
   }
 }
@@ -61,7 +56,6 @@ class MyHomePage extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(title),
-          centerTitle: true,
         ),
         body: Center(
           child: Padding(
@@ -107,7 +101,6 @@ class FlashcardRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<FlashcardsState>();
     return Container(
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.secondaryContainer,
@@ -116,18 +109,10 @@ class FlashcardRow extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
               "$term  |  $definition",
               style: TextStyle(color: Colors.white),
-            ),
-            IconButton(
-              icon: Icon(Icons.delete),
-              color: Colors.red,
-              onPressed: () {
-                appState.removeFlashcard(term);
-              },
             ),
           ],
         ),
