4014127380e9b6786c473d440bb04c0cd4e0750c
Austin Aluzzi <aaaluzzi@cpp.edu>
2023-08-06 13:11:32 -0700

    Use basic material app template

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,32 @@ void main() {
   runApp(
     ChangeNotifierProvider(
       create: (context) => FlashcardsState(),
-      child: const MainApp(),
+      child: const MyApp(),
     ),
   );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flashcards',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flashcards Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +51,10 @@ class MainApp extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(title),
+        ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
